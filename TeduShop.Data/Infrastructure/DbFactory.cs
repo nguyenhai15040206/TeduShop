d310194c601e35a69da06b2f04c12880a604dd59
NguyenTanHai <nguyenhai15040206@gmail.com>
2022-04-30 17:38:08 +0700

    Commit add entity code first

---
+++ TeduShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private TeduShopDbContext _dbContext;
+        public TeduShopDbContext Init()
+        {
+            return _dbContext ?? (_dbContext = new TeduShopDbContext());
+        }
+        protected override void DisposeCore()
+        {
+            if(_dbContext != null)
+            {
+                _dbContext.Dispose();
+            }
+        }
+    }
+}
