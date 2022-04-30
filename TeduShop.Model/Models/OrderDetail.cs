dcb4d512f5d05decc9dfb7616aa2383e80277356
NguyenTanHai <nguyenhai15040206@gmail.com>
2022-04-30 16:54:48 +0700

    Commit Add models domain entity

---
+++ TeduShop.Model/Models/OrderDetail.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TeduShop.Model.Abstract;
+
+namespace TeduShop.Model.Models
+{
+    [Table("OrderDetails")]
+    public class OrderDetail 
+    {
+        [Key]
+        [Column(Order = 1)]
+        public int OrderID { set; get; }
+
+        [Key]
+        [Column(Order = 2)]
+        public int ProductID { set; get; }
+
+        public int Quantity { set; get; }
+
+        public decimal Price { set; get; }
+
+        [ForeignKey("OrderID")]
+        public virtual Order Order { set; get; }
+
+        [ForeignKey("ProductID")]
+        public virtual Product Product { set; get; }
+    }
+}
